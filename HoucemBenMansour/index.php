7be05a5ac43b301ccc70679864bf1bbb5d52aba7
webdesigner4every-one <65117938+webdesigner4every-one@users.noreply.github.com>
2020-05-10 14:16:21 +0100

    Add files via upload

---
+++ HoucemBenMansour/index.php
@@ -0,0 +1,268 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>HoucemGym</title>
+
+  <!-- Font Awesome Icons -->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet">
+  <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
+
+  <!-- Plugin CSS -->
+  <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+<!-- zoom out CSS -->
+
+  <!-- Theme CSS - Includes Bootstrap -->
+  <link href="css/creative.min.css" rel="stylesheet">
+</head>
+
+<body id="page-top">
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">HOUCEM GYM</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto my-2 my-lg-0">
+        	<li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="login.html">
+            connexion</li>
+            <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="inscription.html">
+            créer un compte</li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#about">
+            à propos</li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#services">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#portfolio">photo</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Masthead -->
+  <header class="masthead">
+    <div class="container h-100">
+      <div class="row h-100 align-items-center justify-content-center text-center">
+        <div class="col-lg-10 align-self-end">
+          <h1 class="text-uppercase text-white font-weight-bold">bienvenu à houcem gym</h1>
+          <hr class="divider my-4">
+        </div>
+        <div class="col-lg-8 align-self-baseline">
+          <p class="text-white-75 font-weight-light mb-5">salle de sport et de fitness</p>
+          <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">en savoir plus</a>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- About Section -->
+  <section class="page-section bg-primary" id="about">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-8 text-center">
+          <h2 class="text-white mt-0">nos espace</h2>
+          <hr class="divider light my-4">
+          <p class="text-white-50 mb-4">profiter de nos éspace bien équipés</p>
+          <a class="btn btn-light btn-xl js-scroll-trigger" href="#services">Commencer!</a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Services Section -->
+  <section class="page-section" id="services">
+    <div class="container">
+      <h2 class="text-center mt-0">À votre service</h2>
+      <hr class="divider my-4">
+      <div class="row">
+        <div class="col-lg-3 col-md-6 text-center">
+          <div class="zoom-out">
+            <img src="img\portfolio\thumbnails\biceps.png" height="100px" alt="biceps"></img>
+            <h3 class="h4 mb-2">Musculation</h3>
+            <p class="text-muted mb-0">Suivez nos parcours d’exercices pour apprenez l'importance de l'échauffement et de bien utiliser le mirroir pour contrôler beaucoup plus facilement le placement de votre corps et aussi la trajectoire du mouvement.</p>
+          </div>
+        </div>
+        <div class="col-lg-1 col-md-3 text-center">
+          
+        </div>
+        <div class="col-lg-3 col-md-6 text-center">
+          <div class="mt-5">
+            <img src="img\portfolio\thumbnails\cardio.jpg" height="100px" alt="cardio"></img>
+            <h3 class="h4 mb-2">Cardio-training</h3>
+            <p class="text-muted mb-0">Profitez de notre espace pour améliorer votre condition physique, le souffle, la récupération, faciliter la perte de poids et brûler des graisses. </p>
+          </div>
+        </div>
+
+          <div class="mt-5">
+          </div>
+ 
+        <div class="col-lg-5 col-md-8 text-center">
+          <div class="mt-5">
+           <img  src="img\portfolio\thumbnails\fitness.png" height="150px" alt="fitness"></img>
+            <h3 class="h4 mb-2">Espace Fitness</h3>
+            <p class="text-muted mb-0">Profitez de notre espace de Fitness. Vous vouliez transpirer, danser, vous affiner, vous muscler, vous défouler, vous détendre, il y en a pour tous les goûts, tous les besoins et toutes les envies.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Portfolio Section -->
+  <section id="portfolio">
+    <div class="container-fluid p-0">
+      <div class="row no-gutters">
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/11.jpg">
+            <img class="img-fluid" src="img/portfolio/thumbnails/11.jpg" alt="">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+                
+              </div>
+              <div class="project-name">
+                Le sport
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/12.jpg">
+            <img class="img-fluid" src="img/portfolio/thumbnails/12.jpg" alt="">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+                
+              </div>
+              <div class="project-name">
+                renforce
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/13.jpg">
+            <img class="img-fluid" src="img/portfolio/thumbnails/13.jpg" alt="">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+                
+              </div>
+              <div class="project-name">
+                 le cœur
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/14.jpg">
+            <img class="img-fluid" height="100%" src="img/portfolio/thumbnails/14.jpg" alt="">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+                
+              </div>
+              <div class="project-name">
+                et
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/15.jpg">
+            <img class="img-fluid" src="img/portfolio/thumbnails/15.jpg" alt="">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+                
+              </div>
+              <div class="project-name">
+               régule
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/16.jpg">
+            <img class="img-fluid" src="img/portfolio/thumbnails/16.jpg" alt="">
+            <div class="portfolio-box-caption p-3">
+              <div class="project-category text-white-50">
+               
+              </div>
+              <div class="project-name">
+                la tension
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Call to Action Section -->
+  <section class="page-section bg-dark text-white">
+    <div class="container text-center">
+      <h2 class="mb-4">inscrir ici !</h2>
+      <a class="btn btn-light btn-xl" href="">Connexion</a>
+    </div>
+  </section>
+
+  <!-- Contact Section -->
+  <section class="page-section" id="contact">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-8 text-center">
+          <h2 class="mt-0">Contacter Nous!</h2>
+          <hr class="divider my-4">
+          <p class="text-muted mb-5"></p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-4 ml-auto text-center mb-5 mb-lg-0">
+          <i class="fas fa-phone fa-3x mb-3 text-muted"></i>
+          <div>+116 58725258</div>
+        </div>
+        <div class="col-lg-4 mr-auto text-center">
+          <i class="fas fa-envelope fa-3x mb-3 text-muted"></i>
+          <!-- Make sure to change the email address in anchor text AND the link below! -->
+          <a class="d-block" href="mailto:contact@yourwebsite.com">contact@houcem-gym.com</a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Footer -->
+  <footer class="bg-light py-5">
+    <div class="container">
+      <div class="small text-center text-muted">Copyright &copy; 2019 - HOUCEM GYM</div>
+    </div>
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/creative.min.js"></script>
+</body>
+
+</html>
